628c00a462a0c8c3678587e594a7bc119dd80111
Rich Minchuk <rjminchuk@icloud.com>
2026-03-27 10:51:26 -0500

    Revert "Added Zone output to other domains"
    This reverts commit 4a3b1d724847b2bf4911dd6c8cbf4730dcdb2179.

--- trunkform.com/infra/modules/registered_domain/main.tf
+++ trunkform.com/infra/modules/registered_domain/main.tf
@@ -14,10 +14,6 @@ output "domain_name" {
   value = var.domain_name
 }
 
-output "zone_id" {
-  value = aws_route53_zone.this.zone_id
-}
-
 resource "aws_route53_zone" "this" {
   name = var.domain_name
 }
