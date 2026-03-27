4a3b1d724847b2bf4911dd6c8cbf4730dcdb2179
Rich Minchuk <rjminchuk@icloud.com>
2026-03-27 09:57:28 -0500

    Added Zone output to other domains

--- trunkform.io/infra/modules/registered_domain/main.tf
+++ trunkform.io/infra/modules/registered_domain/main.tf
@@ -14,6 +14,10 @@ output "domain_name" {
   value = var.domain_name
 }
 
+output "zone_id" {
+  value = aws_route53_zone.this.zone_id
+}
+
 resource "aws_route53_zone" "this" {
   name = var.domain_name
 }
